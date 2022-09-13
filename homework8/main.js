ed807a25e94a5a8532b5b5ff1dd04e98e40555ac
yustyn yurii <orgneorgne@.meta.ua>
2022-09-13 19:11:04 +0300

    added homework8

---
+++ homework8/main.js
@@ -0,0 +1,72 @@
+class Student {
+   constructor(university, course, fullName, marks) {
+      this.university = university
+      this.course = course
+      this.fullName = fullName
+      this.marks = marks
+      this.dismiss = false
+   }
+   getInfo() {
+      return `Студент ${this.course}го курсу, ${this.university}, ${this.fullName} `
+   }
+   get marksArr() {
+      return !this.dismiss ? this.marks : null
+   }
+   set marksArr(mark) {
+      if (!this.dismiss) {
+         return this.marks.push(mark)
+      } else {
+         return null
+      }
+   }
+   getAverageMark() {
+      let sum = this.marks.reduce((acc, item) => acc + item)
+      return sum / (this.marks).length
+   }
+   getDismiss() {
+      this.dismiss = true
+      console.log('Студента виключено')
+   }
+   getRecover() {
+      this.dismiss = false
+      console.log("Студента поновлено, тепер можна ставити і переглядати його оцінки");
+   }
+}
+const ostap = new Student("Вища школа психотерапії м.Одеса", "1", "Остап Бендер", [5, 4, 4, 5])
+//Завдання 1
+console.log(ostap)
+//Завдання 2
+console.log(ostap.getInfo())
+//Завдання 3
+console.log(ostap.marksArr)
+//Завдання 4
+ostap.marksArr = 5
+console.log(ostap.marksArr)
+//Завдання 5
+console.log(ostap.getAverageMark());
+//Завдання 6
+ostap.getDismiss()
+ostap.marksArr = 6
+console.log(ostap.marksArr);
+//Завдання 7
+ostap.getRecover()
+ostap.marksArr = 7
+console.log(ostap.marksArr);
+
+console.log("////////////////////////////////////////ADWANCED//////////////////////////////////////////////////")
+
+class BudgetStudent extends Student {
+   constructor(scholarship, university, course, fullName, marks) {
+      super(university, course, fullName, marks)
+      this.scholarship = scholarship
+      this.dismiss = false
+      this.getScholarship = setInterval(() => {
+         return !this.dismiss && this.getAverageMark() >= 4 ? console.log('Ви отримали 1400 грн стипендії') : console.log('Лузер')
+      }, 30000)
+   }
+
+}
+
+const oleg = new BudgetStudent(true, "ТНЕУ м.Тернопіль", 2, 'Oleg Babay', [4, 5, 5, 5, 4])
+
+
