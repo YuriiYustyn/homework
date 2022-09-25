95fcc8023460af770dfb16f1b8a5f4a79092b8ca
yustyn yurii <orgneorgne@.meta.ua>
2022-09-25 13:48:55 +0300

    corrected the comment

--- homework9/main.js
+++ homework9/main.js
@@ -4,20 +4,13 @@ function randomColor() {
 }
 
 function generateBlocks() {
-   let bigSquare = document.querySelector(".test")
-   const bigSquareSize = 50
-   const heightWidth = 5
-   bigSquare.style.width = `${bigSquareSize * heightWidth}px`
-   bigSquare.style.height = `${bigSquareSize * heightWidth}px`
-   bigSquare.style.display = `flex`
-   bigSquare.style.flexWrap = 'wrap'
+   let bigSquare = document.querySelector(".test")  
+   bigSquare.classList.add('bigSquare')
    bigSquare.innerHTML=''
-   for (let i = 0; i < heightWidth * heightWidth ; i++) {
+   for (let i = 0; i < 25 ; i++) {
       let square = document.createElement("div")
-      square.style.width = `${bigSquareSize}px`
-      square.style.height = `${bigSquareSize}px`
-      square.style.background = `${randomColor()}`
-      square.style.borderRadius = '3px'
+      square.classList.add('smallSquare')
+      square.style.background = randomColor()
       bigSquare.append(square)
    }
 }
