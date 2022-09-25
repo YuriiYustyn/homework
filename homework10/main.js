d09dda9e2df7aaa69f1468d651d566df60588945
yustyn yurii <orgneorgne@.meta.ua>
2022-09-25 18:42:45 +0300

    impro

--- homework10/main.js
+++ homework10/main.js
@@ -4,21 +4,22 @@ const divButton = document.querySelectorAll('.el')
 document.body.addEventListener("keydown", replaceAll)
 function replaceAll(e) {
    let keyCodeEvent = e.keyCode
-   for (let i = 0; i < audios.length; i++) {
-      if (audios[i].id == keyCodeEvent) {
-         audios[i].currentTime = 0
-         audios[i].play()
-         divButton[i].className = "elDown"
-         setTimeout(function(){ divButton[i].className = 'el' }, 300)
+   audios.forEach((el, ind) => {
+      if (el.id == keyCodeEvent) {
+         elementReproduction(el, ind)
       }
-   }
-}
-
-for (let i = 0; i < divButton.length; i++) {
-   divButton[i].addEventListener("click", function (e) {
-      audios[i].currentTime = 0
-      audios[i].play()
-      divButton[i].className = "elDown"
-      setTimeout(function () { divButton[i].className = 'el' }, 300)
    })
 }
+
+function elementReproduction(item, i) {
+   item.currentTime = 0
+   item.play()
+   divButton[i].classList.toggle("elDown")
+   setTimeout(() => divButton[i].className = 'el' , 300)
+}
+
+divButton.forEach((el, ind) => {
+   el.addEventListener("click", () => 
+      elementReproduction(audios[ind], ind)
+   )
+})
